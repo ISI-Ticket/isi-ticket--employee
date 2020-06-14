b2d455a3024b603b4ff05363e10315c4a4fb2546
Eduardo Ribeiro <eagr1999@gmail.com>
2020-06-14 18:43:45 +0100

    qrcode, perfil finished

--- front_end/vendor/js/profile.js
+++ front_end/vendor/js/profile.js
@@ -1,25 +1,98 @@
-windowonload = getProfile();
-
-function getProfile(){
+window.onload = () => {
+    getProfile();
+  
+    let api = JSON.parse(localStorage.getItem('api'));
+  
+    const signOutBtn = document.getElementById('signOutBtn');
+  
+    if (api === 'google') {
+  
+      let script = document.createElement('script');
+      console.log("carreguei")
+      script.src = 'https://apis.google.com/js/platform.js?onload=initialize';
+      script.async = 'async';
+      script.defer = 'defer';
+  
+      document.head.appendChild(script);
+  
+    }
+  
+    if (api === 'facebook') {
+      window.fbAsyncInit = function () {
+        FB.init({
+          appId: '253404675657890',
+          autoLogAppEvents: true,
+          xfbml: true,
+          version: 'v7.0'
+        });
+      };
+  
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/pt_PT/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+  
+      signOutBtn.addEventListener('click', () => {
+        signOutFacebook();
+      });
+  
+    }
+  
+  }
+  
+  function getProfile() {
     let profile = JSON.parse(localStorage.getItem('profile'));
-    console.log(profile);
     document.getElementById("email").value = profile.email;
     document.getElementById("firstname").value = profile.firstname;
     document.getElementById("lastname").value = profile.lastname;
+    document.getElementById("nif").value = profile.nif;
     document.getElementById("phone").value = profile.phone;
     document.getElementById("address").value = profile.address;
     document.getElementById("city").value = profile.city;
     document.getElementById("zip").value = profile.zip;
-
-}
-
-
-function signOut(){
-
+  
+  }
+  
+  function onLoad() {
+    console.log("terminar")
+    gapi.load('auth2', function () {
+      gapi.auth2.init();
+    });
+  }
+  
+  const signOutGoogle = () => {
+    console.log("terminar")
     let auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
+    auth2.signOut().then(() => {
+      localStorage.clear();
+      window.location.href = '../../index.html';
+    });
+  
+  }
+  
+  const signOutFacebook = () => {
+    FB.logout((response) => {
+      localStorage.clear();
+      console.log(response);
+      window.location.href = '../../index.html';
+    });
+  }
+  
+  function initialize() {
+    console.log("inicializei")
+    gapi.load('auth2', function () {
+      auth2 = gapi.auth2.init({
+        client_id: '743510812799-olf9sq5n33lqvp9reigo9tb11itfucmo.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile'
       });
-
-    window.location.href='../../index.html';
-}
+    });
+  
+    signOutBtn.addEventListener('click', () => {
+      signOutGoogle();
+    });
+  
+  }
--- front_end/vendor/js/signin.js
+++ front_end/vendor/js/signin.js
@@ -2,14 +2,40 @@ window.onload = () => {
 
     startApp();
 
-    let facebookBtn = document.getElementById('facebookBtn');
+    fetch('https://connect.facebook.net/en_US/sdk.js')
+        .then(function () {
+            window.fbAsyncInit = () => {
+                FB.init({
+                    appId: '253404675657890',
+                    autoLogAppEvents: true,
+                    xfbml: true,
+                    version: 'v7.0'
+                });
+            };
 
-    facebookBtn.addEventListener('click', (e) => {
-        e.preventDefault();
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) { return; }
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/pt_PT/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
 
-        facebookLogin();
-    });
-} 
+
+            let facebookBtn = document.getElementById('facebookBtn');
+
+            facebookBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                facebookLogin();
+            });
+        })
+        .catch((err) => {
+            console.log(err)
+            console.log('Facebook sdk is NOT allowed:');
+        });
+
+}
 
 const startApp = () => {
     gapi.load('auth2', function () {
@@ -26,64 +52,58 @@ const attachSignin = (element) => {
     auth2.attachClickHandler(element, {},
         function (googleUser) {
             googleUser = googleUser.getBasicProfile();
+            userEmail = googleUser.getEmail();
+            userFirstname = googleUser.getName();
+            userLastName = googleUser.getFamilyName();
 
             let profile = {
-                'email': googleUser.Eu,
-                'firstName': googleUser.GW,
-                'lastName': googleUser.GU,
+                'email': userEmail,
+                'firstName': userFirstname,
+                'lastName': userLastName,
                 'api': 'google'
             }
 
             signInApi(profile);
 
         }, function (error) {
-            if(error.error === 'popup_closed_by_user') {
-                console.log('fechei popup')
+            if (error.error === 'popup_closed_by_user') {
             } else {
                 console.log(error);
             }
         })
 }
 
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '253404675657890',
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v6.0'
-    });
-};
-
-// Load the SDK asynchronously
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 const facebookLogin = () => {
     FB.getLoginStatus((response) => {
         if (response.status === 'connected') {
             FB.api('/me?fields=first_name, last_name, email, picture.type(large)', function (userData) {
-                let data = {
-                    "email": userData.email,
-                    "firstName": userData.first_name,
-                    "lastName": userData.last_name
+                let profile = {
+                    'email': userData.email,
+                    'firstName': userData.first_name,
+                    'lastName': userData.last_name,
+                    'api': 'facebook'
                 }
 
-                signInApi(data);
+                signInApi(profile);
             });
+        } else {
+            FB.login((response) => {
+                if (response.status === 'connected') {
+                    FB.api('/me?fields=first_name, last_name, email, picture.type(large)', function (userData) {
+                        let profile = {
+                            'email': userData.email,
+                            'firstName': userData.first_name,
+                            'lastName': userData.last_name,
+                            'api': 'facebook'
+                        }
+        
+                        signInApi(profile);
+                    });
+                }
+            }, { scope: 'public_profile,email' });
         }
 
     }, { scope: 'public_profile, email' })
-}
-
-function facebookLogout() {
-    FB.logout((response) => {
-        console.log(response);
-    });
 }
 
 function signInApi(profile) {
@@ -92,7 +112,7 @@ function signInApi(profile) {
         email: profile.email
     }
 
-   fetch('https://isi-ticket-api.herokuapp.com/user/signin', {
+    fetch   ('https://isi-ticket-api.herokuapp.com/user/signin', {
         headers: { 'Content-Type': 'application/json' },
         method: 'POST',
         body: JSON.stringify(data)
@@ -103,12 +123,16 @@ function signInApi(profile) {
 
         if (data.exists == true) {
             localStorage.setItem('profile', response);
-            window.location.href = './vendor/pages/qrcode.html'
+            localStorage.setItem('api', JSON.stringify(profile.api));
+            window.location.href = './vendor/pages/perfil.html'
         } else {
-            window.location.href = './vendor/pages/qrcode.html'
-            localStorage.setItem('email', profile.email);
-            localStorage.setItem('firstname', profile.firstName);
-            localStorage.setItem('lastname', profile.lastName);
+            localStorage.setItem("email", profile.email);
+            localStorage.setItem("firstname", profile.firstName);
+            localStorage.setItem("lastname", profile.lastName);
+            localStorage.setItem('api', JSON.stringify(profile.api));
+            window.location.href = './vendor/pages/registar.html'
+
         };
     });
+
 }
