d2ded949d6137989a791da314880ef1558b0df19
Eduardo Ribeiro <eagr1999@gmail.com>
2020-06-14 16:45:06 +0100

    rear camera

--- front_end/vendor/js/scanner.js
+++ front_end/vendor/js/scanner.js
@@ -1,46 +1,56 @@
 let scanner = new Instascan.Scanner({
     video: document.getElementById('preview')
 });
-scanner.addListener('scan', function(content) {
+scanner.addListener('scan', function (content) {
     validateTicket(content);
 });
 Instascan.Camera.getCameras().then(cameras => {
     if (cameras.length > 0) {
-        scanner.start(cameras[0]);
+        let selectedCam = cameras[0];
+
+        console.log(cameras);
+
+        cameras.forEach(camera => {
+            if(camera.name.indexOf('back') != -1) {
+                selectedCam = camera;
+            }
+        })
+
+        scanner.start(selectedCam);
     } else {
         console.error("Não existe câmera no dispositivo!");
     }
 });
 
-
-function validateTicket(content){
+function validateTicket(content) {
     let saleID = content;
-    let data = {saleID};
+    let data = { saleID };
     console.log(data);
-    fetch('http://localhost:5000/employee/validate',{
-        headers: {'Content-Type': 'application/json'},
+    fetch('http://localhost:5000/employee/validate', {
+        headers: { 'Content-Type': 'application/json' },
         method: 'POST',
         credentials: "include",
         body: JSON.stringify(data)
-        }).then(function(res){ return res.json(); 
-        }).then(function(data){
-            console.log(data);
-            showPopup(data);            
-        });
+    }).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        console.log(data);
+        showPopup(data);
+    });
 
 
 }
 
 
-function showPopup(response){
+function showPopup(response) {
     console.log(response.status);
-    if(response.status == false){
+    if (response.status == false) {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Senha validada previamente!',
-          })
-    }else{
+        })
+    } else {
         Swal.fire({
             title: 'Sucesso!',
             text: response.msg,
